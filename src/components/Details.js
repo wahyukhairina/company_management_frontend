056741e447eeb4cf1561faf3d49a5cd626bd2337
wahyu khairina <wkhairina.afzan@gmail.com>
2020-04-19 22:26:28 +0800

    add Details

---
+++ src/components/Details.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Details extends Component {
+
+render() {
+    return (
+        <>
+        <div>
+            DETAILS
+        </div>
+        </>
+    )
+}
+}
+export default Details
